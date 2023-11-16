a5e52836de28e259e72cf501ca0cbf5fd726c626
Yoga Adhi Prasetya <96295892+yogadhipr@users.noreply.github.com>
2023-11-16 20:51:05 +0700

    Add files via upload

---
+++ Testing Shapes/Testing Shapes/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Shapes
+{
+    public class shapes
+    {
+        public double kubus(double a)
+        {
+            return a *a  *a ;
+        }
+
+
+        public double balok(double a, double b, double c)
+        {
+            return a * b * c;
+        }
+
+        public double tabung(double a, double b)
+        {
+            return 22 / 7 * a * a * b;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = Double.Parse(args[0]);
+            double b = Double.Parse(args[1]);
+            double c = Double.Parse(args[2]);
+            
+
+            shapes sh = new shapes();
+
+            double kb = sh.kubus(a);
+            double bl = sh.balok(a, b, c);
+            double tb = sh.tabung(a, b);
+
+            Console.WriteLine("volume kubus = {0}", kb);
+            Console.WriteLine("volume balok = {0}", bl);
+            Console.WriteLine("volume tabung = {0}", tb);
+        }
+    }
+}
---
+++ Testing Shapes/Testing ShapesTests/shapesTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using Shapes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes.Tests
+{
+    [TestFixture()]
+    public class shapesTests
+    {
+        shapes sh = new shapes();
+
+        [Test()]
+        public void kubusTest()
+        {
+            double hasilKb = sh.kubus(5);
+            Assert.AreEqual(125, hasilKb);
+        }
+
+        [Test()]
+        public void balokTest()
+        {
+            double hasilBl = sh.balok(5, 6, 7);
+            Assert.AreEqual(210, hasilBl);
+        }
+
+        [Test()]
+        public void tabungTest()
+        {
+            double hasilTb = sh.tabung(5, 6);
+            Assert.AreEqual(450, hasilTb);
+        }
+
+        [Test()]
+        public void kubusTest1()
+        {
+
+        }
+    }
+}
